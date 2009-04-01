1293ad1bb027a556fc6db3eddef0f11d67d32ca4
Elijah Miller <elijah.miller@gmail.com>
2009-04-01 17:47:17 -0400

    Converting all git_commit_alls to block syntax.

--- template.rb
+++ template.rb
@@ -29,39 +29,35 @@ git_commit_all 'Removing TestUnit.' do
   run "rm -rf test/"
 end
 
-
-gem 'populator', :env => :development
-gem 'faker', :env => :development
-gem "josevalim-rails-footnotes",  :lib => "rails-footnotes", :source => "http://gems.github.com", :env => :development
-plugin 'railmail2', :git => 'git://github.com/theoooo/railmail2.git'
-environment 'ActionMailer::Base.delivery_method = :railmail', :env => :development
-gem 'annotate-models', :lib => 'annotate_models', :env => :development
-plugin 'limerick_rake', :git => "git://github.com/thoughtbot/limerick_rake.git"
-
-git :add => "."
-git :commit => "-a -m 'Added development helpers: populator, faker, rails-footnotes, railmail2, annotate-models and limerick_rake.'"
-
-gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
-gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-gem "newrelic_rpm" # TODO: get a default newrelic.yml
-gem 'authlogic'
-
-git :add => "."
-git :commit => "-a -m 'Added general libraries: paperclip, will_paginate, newrelic_rpm and authlogic.'"
+git_commit_all 'Added development helpers: populator, faker, rails-footnotes, railmail2, annotate-models and limerick_rake.' do
+  gem 'populator', :env => :development
+  gem 'faker', :env => :development
+  gem "josevalim-rails-footnotes",  :lib => "rails-footnotes", :source => "http://gems.github.com", :env => :development
+  plugin 'railmail2', :git => 'git://github.com/theoooo/railmail2.git'
+  environment 'ActionMailer::Base.delivery_method = :railmail', :env => :development
+  gem 'annotate-models', :lib => 'annotate_models', :env => :development
+  plugin 'limerick_rake', :git => "git://github.com/thoughtbot/limerick_rake.git"
+end
 
 
-# TODO: generate hoptoad api key
-plugin 'hoptoad_notifier', :git => "git://github.com/thoughtbot/hoptoad_notifier.git"
-plugin 'kristopher-asset-version', :git => 'git://github.com/kristopher/asset-version'
+git_commit_all 'Added general libraries: paperclip, will_paginate, newrelic_rpm and authlogic.' do
+  gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
+  gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  gem "newrelic_rpm" # TODO: get a default newrelic.yml
+  gem 'authlogic'
+end
 
-git :add => "."
-git :commit => "-a -m 'Adding production helpers: hoptoad_notifier and asset-version.'"
+git_commit_all 'Adding production helpers: hoptoad_notifier and asset-version.' do
+  # TODO: generate hoptoad api key
+  plugin 'hoptoad_notifier', :git => "git://github.com/thoughtbot/hoptoad_notifier.git"
+  plugin 'kristopher-asset-version', :git => 'git://github.com/kristopher/asset-version'
+end
 
+git_commit_all 'Capifying.' do
+  capify!
+end
 
-capify!
-git_commit_all 'Capifying.'
-
-# google analytics?
+# TODO: google analytics?
 
 git_commit_all 'Adding concerns directory.' do
   file('app/concerns/.gitignore')
